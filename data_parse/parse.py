dcee67d80ee1bb611c3388644872d3bb6eeb4214
Тимур <timur.scepanov@gmail.com>
2025-05-09 16:01:40 +0300

    asdf

--- data_parse/parse.py
+++ data_parse/parse.py
@@ -31,7 +31,6 @@ def getProfiles(username, password):
     try:
         wait.until(EC.url_contains("https://www.e-klase.lv/"))
 
-
         submitButton = wait.until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn-success[data-btn='submit']"))
         )
